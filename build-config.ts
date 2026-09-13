acb89807bee4e4052bbeec02ea6c92837eb0057b
javadbat <javadbat@gmail.com>
2026-09-14 01:27:42 +0330

    chore: update cire/i18n config

--- build-config.ts
+++ build-config.ts
@@ -16,7 +16,7 @@ export const webComponentList: WebComponentBuildConfig[] = [
       "jb-validation": "JBValidation",
       "jb-core":"JBCore",
       "jb-core/theme":"JBCoreTheme",
-      "jb-core/i18n":"JBCoreI18N"
+      "jb-core/i18n":"JBCoreI18n"
     },
   },
 ];
